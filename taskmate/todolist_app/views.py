4c0994478844428eea4bd5ffbefe5d5a59c09ddd
Tsilkostas <kostas.gv@gmail.com>
2023-02-04 14:54:32 +0200

    playing-with-jinja

--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -3,7 +3,21 @@ from django.http  import HttpResponse
 
 def todolist(request):
     context={
-        'welcome_text':'Welcome From Jinga'
+        'welcome_text':'Welcome to To Do List page'
         
     }
     return render(request, 'todolist.html',context)
+
+def contact(request):
+    context={
+        'contact_text':'Welcome to Contact page'
+        
+    }
+    return render(request, 'contact.html',context)
+
+def about(request):
+    context={
+        'about_text':'Welcome to About page'
+        
+    }
+    return render(request, 'about.html',context)

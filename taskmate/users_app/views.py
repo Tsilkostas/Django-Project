c46932333415ca8fd13e6d828cb9a80b72ff7fb1
Tsilkostas <kostas.gv@gmail.com>
2023-02-26 15:08:43 +0200

    add registration form

--- taskmate/users_app/views.py
+++ taskmate/users_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
 
 def register(request):
-    return HttpResponse("Users_app is working")
+    register_form = UserCreationForm()
+    return render(request, 'register.html', {'register_form':register_form})

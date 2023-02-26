f9d7ece268737813d2258a09f0b5379e73d997b7
Tsilkostas <kostas.gv@gmail.com>
2023-02-26 15:56:19 +0200

    add register views

--- taskmate/users_app/views.py
+++ taskmate/users_app/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def register(request):
-    register_form = UserCreationForm()
+    """ This function adds functionality to the form of the registration page. """
+    if request.method=="POST":
+        register_form = UserCreationForm(request.POST)
+        if register_form.is_valid():
+            # register_form.save()
+            messages.success(request, ("New User Account Created, Login To Get Started"))
+            return redirect('todolist')
+    else:   
+         
+        register_form = UserCreationForm()
     return render(request, 'register.html', {'register_form':register_form})

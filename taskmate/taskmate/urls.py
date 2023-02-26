19d3e437c95b82a1aa03c25262b1f6b9fcdbb519
Tsilkostas <kostas.gv@gmail.com>
2023-02-26 14:59:25 +0200

    create user section

--- taskmate/taskmate/urls.py
+++ taskmate/taskmate/urls.py
@@ -20,7 +20,9 @@ from todolist_app import views as todolist_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todolist/',include('todolist_app.urls')),
+    path('account/',include('users_app.urls')),
     path('contact', todolist_views.contact, name='contact'),
     path('about', todolist_views.about, name='about'),
     path('', todolist_views.index, name='index')
 ]
+

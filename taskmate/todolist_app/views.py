016e585f7a87ae41232a5b5d41d2880b7c00cfbc
Tsilkostas <kostas.gv@gmail.com>
2023-02-28 00:19:48 +0200

    add page restrictions

--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -4,7 +4,9 @@ from todolist_app.models import TaskList
 from todolist_app.forms import TaskForm
 from django.contrib import messages
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def todolist(request):
     if request.method=="POST":
         form = TaskForm(request.POST or None)
@@ -20,6 +22,7 @@ def todolist(request):
         all_tasks = paginator.get_page(page)
         
         return render(request, 'todolist.html',{'all_tasks':all_tasks})
+
 
 def contact(request):
     context={
@@ -42,11 +45,13 @@ def index(request):
     }
     return render(request, 'index.html',context)
 
+@login_required
 def delete_task(request, task_id):
     task = TaskList.objects.get(pk=task_id)
     task.delete()
     return redirect('todolist')
 
+@login_required
 def edit_task(request, task_id):
     if request.method=="POST":
         task = TaskList.objects.get(pk=task_id)
@@ -61,13 +66,14 @@ def edit_task(request, task_id):
         
         return render(request, 'edit.html',{'task_obj':task_obj})
     
-    
+@login_required    
 def complete_task(request, task_id):
     task = TaskList.objects.get(pk=task_id)
     task.done = True
     task.save()
     return redirect('todolist')
 
+@login_required
 def pending_task(request, task_id):
     task = TaskList.objects.get(pk=task_id)
     task.done = False

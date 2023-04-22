577dfab400a8a9412cff901c049a64509dba5950
Tsilkostas <kostas.gv@gmail.com>
2023-04-22 19:31:45 +0300

    add gitignore and requirements

--- taskmate/todolist_app/models.py
+++ taskmate/todolist_app/models.py
@@ -8,6 +8,9 @@ class TaskList(models.Model):
     task = models.CharField(max_length=300)
     done = models.BooleanField(default=False)
     
+    class Meta:
+        ordering = ['id']
+    
     """ Method to make Task list object(from admin panel) a string """
     def __str__(self):
         return self.task + " - " + str(self.done)

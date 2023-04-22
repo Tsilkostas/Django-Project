77b46f46eb812dd17f8224ec31d7c72e0c2412aa
Tsilkostas <kostas.gv@gmail.com>
2023-04-22 19:05:03 +0300

    another commit

--- taskmate/todolist_app/models.py
+++ taskmate/todolist_app/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 """ Creation of database models """
 class TaskList(models.Model):
+    id = models.AutoField(primary_key=True)
     manage = models.ForeignKey(User, on_delete= models.CASCADE, default= None)
     task = models.CharField(max_length=300)
     done = models.BooleanField(default=False)
